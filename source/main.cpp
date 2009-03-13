10c220f85083a98dca1aed9b60dc6e7c70f24b62
kyle2 <kyle2@ac2c0e10-25e9-44ee-ad31-64d50a0de64a>
2009-03-13 23:07:33 +0000

    Fixing up map understanding and comming up with something ... cool
    git-svn-id: file://localhost/home/share/Development/Subversion_repos/RazzleGBA@15 ac2c0e10-25e9-44ee-ad31-64d50a0de64a

--- source/main.cpp
+++ source/main.cpp
@@ -39,7 +39,10 @@ extern "C" {
 #include "casper2_pcx.h"
 #include "toolbox.h"
 #include "test_map.h"
+#include "test_map2.h"
 #include "test_bg.h"
+#include "brin.h"
+
 #ifdef	__cplusplus
 }
 #endif
@@ -694,10 +697,9 @@ void init_map()
         }
         ,
             {{0x00000000, 0x00100100, 0x01100110, 0x00011000,
-                 0x00011000, 0x01100110, 0x00100100, 0x00000000}
-            }
-        ,
-    } ;
+                 0x00011000, 0x01100110, 0x00100100, 0x00000000}},
+
+    };
 
     tile_mem[CBB_0][0]= tiles[0];
     tile_mem[CBB_0][1]= tiles[1];
@@ -714,84 +716,218 @@ void init_map()
     SCR_ENTRY *pse = bg0_map;
     for(ii=0; ii<4; ii++)
                 for(jj=0; jj<32*32; jj++)
-                            *pse++= SE_PALBANK(ii) ;
+                            *pse++= SE_PALBANK(ii) |1 ;
     //| 0;
 }
 
 void init_map_test()
 {
-   
 
     // initialize a background
-    REG_BG0CNT= BG_CBB(CBB_0) | BG_SBB(SBB_0) | BG_REG_64x64;
+    REG_BG0CNT= BG_CBB(CBB_0) | BG_SBB(SBB_0) | BG_REG_32x32 |BG_4BPP;
     REG_BG0HOFS= 0;
     REG_BG0VOFS= 0;
 
-    // create the 2 tiles: 
-    u32 tile_rows[2*TEST_MAP_WIDTH];
+    //    // create the tiles: 
+    //    u16 tile_rows[TEST_MAP_HEIGHT*2] __attribute__((aligned(4)));
+    //
+    //    u16* current_tile __attribute__((aligned(4))) = tile_rows;
+    //
+    //    // Create tiles
+    //    int y =0;
+    //    while(y < TEST_MAP_HEIGHT*TEST_MAP_WIDTH && 
+    //            current_tile != &(tile_rows[TEST_MAP_HEIGHT*2]))
+    //    {
+    //
+    //        *current_tile = 0;
+    //        // Move to top nibble in u16
+    //        *current_tile |= ((u16)test_map_data[y++]) << (3*4);
+    //        *current_tile |= ((u16)test_map_data[y++]) << (2*4);
+    //        *current_tile |= ((u16)test_map_data[y++]) << (1*4);
+    //        *current_tile |= ((u16)test_map_data[y++]) << (0*4);
+    //
+    //        ++current_tile;
+    //
+    //    }
 
+    // create the tiles: 
+    u32 tile_rows[TEST_MAP_HEIGHT*2] __attribute__((aligned(4)));
     // Create tiles
-    for(int i = 0; i < TEST_MAP_HEIGHT; i++)
+    for(int y = 0; y < TEST_MAP_HEIGHT; y++)
     {
 
         // Initialize tile row
-        tile_rows[i] = 0;
+        tile_rows[y] = 0;
 
         // Append each byte to the tile (as a nibble 
         //      in the word/tile_row)
-        for(int a = TEST_MAP_WIDTH; a > 0; a--)
+        for(int x = TEST_MAP_WIDTH; x > 0; x--)
         {
 
-            tile_rows[i] |= 
-                (test_map_data[
-                 (i*TEST_MAP_WIDTH) + TEST_MAP_WIDTH-a] << (4*a));
+            // Append the each row's column (1byte) to a 32byte integer
+            tile_rows[y] |= 
+                (test_map_data[(y*TEST_MAP_WIDTH) + TEST_MAP_WIDTH-x] 
+                 // Shift by 4 bits to the left for each pixel
+                 //     X represents how many nibble spaces to shift left 
+                 //     (if the entire width was used, it would shift off the
+                 //     front of the u32, so x-1 is used.
+                << (4*(x-1)));
 
-        }
+         }
 
-    }
+     }
 
-    TILE* tiles = (TILE*)tile_rows;
+     memcpy(&tile_mem[0][0], tile_rows, TEST_MAP_HEIGHT*2*sizeof(u16));
+     //memcpy(&tile_mem[0][0], brinTiles, brinTilesLen);
+     //memcpy(&tile_mem[0][0], test_map2Tiles, test_map2TilesLen);
 
-    tile_mem[CBB_0][0]= tiles[0];
-    tile_mem[CBB_0][1]= tiles[1];
 
-    // create a palette
-    for(int i = 0; i  < 16; i++)
-    {
+     //TILE* tiles = (TILE*)tile_rows;
+     //    for(int i = 0; 
+     //        i < TEST_MAP_HEIGHT/TEST_MAP_WIDTH; i++)
+     //    {
+     //
+     //        tile_mem[CBB_0][i]= tiles[i];
+     //
+     //        //        if(i == 4)
+     //        //        {
+     //        //
+     //        //            //u32* tile_t = (u32*)&(tile_mem[CBB_0][i]);
+     //        //            u32* tile_t = (u32*)&(tiles[i]);
+     //        //            Debug::printSetup();	     
+     //        //
+     //        //            iprintf("\x1b[1;0H Tile %d:",i );
+     //        //            iprintf("\x1b[2;0H 0x%08x", tile_t[0]);
+     //        //            iprintf("\x1b[3;0H 0x%08x", tile_t[1]);
+     //        //            iprintf("\x1b[4;0H 0x%08x", tile_t[2]);
+     //        //            iprintf("\x1b[5;0H 0x%08x", tile_t[3]);
+     //        //            iprintf("\x1b[6;0H 0x%08x", tile_t[4]);
+     //        //            iprintf("\x1b[7;0H 0x%08x", tile_t[5]);
+     //        //            iprintf("\x1b[8;0H 0x%08x", tile_t[6]);
+     //        //            iprintf("\x1b[9;0H 0x%08x", tile_t[7]);
+     //        //
+     //        //            iprintf("\x1b[10;0H Prev Tile Address 0x%08x:",&tile_mem[CBB_0][i-1] );
+     //        //            iprintf("\x1b[11;0H Tile Address 0x%08x:",&tile_mem[CBB_0][i] );
+     //        //            iprintf("\x1b[12;0H BG0_map Address 0x%08x:",bg0_map);
+     //        //            while(1);
+     //        //        }
+     //
+     //    }
 
-        // Convert Gimp colors from 8bit to 4bit (255 to 31 max)
-        //        int color0 = (int)(test_map_pallet[i][0]*(31.0/255.0);
-        //        int color1 = (int)(test_map_pallet[i][1]*(31.0/255.0);
-        //        int color2 = (int)(test_map_pallet[i][2]*(31.0/255.0);
-        int color0 = test_map_pallet[i][0] >> 3;
-        int color1 = test_map_pallet[i][1] >> 3;
-        int color2 = test_map_pallet[i][2] >> 3;
+     if(TEST_MAP_HEIGHT/TEST_MAP_WIDTH != 5)
+     {
 
-        pal_bg_bank[0][i]= RGB15(color0, color1,color2);
+         Debug::printSetup();	     
 
-    }
+         iprintf("\x1b[1;0H Oh, not rendering 5 tiles?: %d", 
+                 TEST_MAP_HEIGHT/TEST_MAP_WIDTH);
+         //    iprintf("\x1b[1;0H OAMCopy[6].a[0]: %04x", 
+         //            Sprites::OAMCopy[6].attribute[0]);
 
-    //    // create a palette
-    //    pal_bg_bank[0][0]= RGB15((int)(31/2),  0,  0);
-    //    pal_bg_bank[0][1]= RGB15(31,  0,  0);
-    //    pal_bg_bank[0][2]= RGB15( 0, 31,  0);
-    //    pal_bg_bank[0][3]= RGB15( 0,  0, 31);
-    //    pal_bg_bank[0][4]= RGB15(16, 16, 16);
+     }
 
-    // Create a map: four contingent blocks of 
-    //   0x0000, 0x1000, 0x2000, 0x3000.
-    SCR_ENTRY *pse = bg0_map;
-    for(int i = 0; i < TEST_MAP_SIZE; i++)
-    {
+     // create a palette
+     for(int i = 0; i  < 256; i++)
+     {
 
-        *pse++= (TEST_MAP[i]) || SE_PALBANK(0);
+         // Convert Gimp colors from 8bit to 4bit (255 to 31 max)
+         //   Equivalent to the following, but using more efficient shifts
+         //        // Red
+         //        int color0 = (int)(test_map_pallet[i][0]*(32.0/256.0);
+         //        // Green
+         //        int color1 = (int)(test_map_pallet[i][1]*(32.0/256.0);
+         //        // Blue 
+         //        int color2 = (int)(test_map_pallet[i][2]*(32.0/256.0);
+         int color0 = test_map_pallet[i][0] >> 3;
+         int color1 = test_map_pallet[i][1] >> 3;
+         int color2 = test_map_pallet[i][2] >> 3;
 
-    }
+         pal_bg_bank[0][i]= RGB15(color0, color1,color2);
 
-    //    for(ii=0; ii<4; ii++)
-    //                for(jj=0; jj<32*32; jj++)
-    //                            *pse++= SE_PALBANK(ii) ;
-        //| 0;
+     }
+
+     //memcpy(&(pal_bg_bank[0][0]),brinPal, brinPalLen);
+     //memcpy(&(pal_bg_bank[0][0]),test_map2Pal, test_map2PalLen);
+
+     //    // create a palette
+     //    pal_bg_bank[0][0]= RGB15((int)(31/2),  0,  0);
+     //    pal_bg_bank[0][1]= RGB15(31,  0,  0);
+     //    pal_bg_bank[0][2]= RGB15( 0, 31,  0);
+     //    pal_bg_bank[0][3]= RGB15( 0,  0, 31);
+     //    pal_bg_bank[0][4]= RGB15(16, 16, 16);
+
+
+     // Create a map: four contingent blocks of 
+     //   0x0000, 0x1000, 0x2000, 0x3000.
+     SCR_ENTRY *pse = bg0_map;
+     u16 tile_map[TEST_MAP_SIZE];
+
+     for(int i = 0; i < TEST_MAP_SIZE; i++)
+     {
+
+         //pse[i] = ((u16)TEST_MAP[i] ) | SE_PALBANK(0) ;
+         tile_map[i] = ((u16)TEST_MAP[i] ) | SE_PALBANK(0) ;
+
+         if(i & 0x0001)
+         {
+
+             //*pse |= SE_VFLIP;
+             //*pse |= SE_HFLIP;
+
+         }
+
+         //        if(TEST_MAP[i] > 1)
+         //        {
+         //
+         //            Debug::printSetup();	     
+         //
+         //            iprintf("\x1b[1;0H Tile was %d:",TEST_MAP[i] );
+         //
+         //
+         //        }
+
+     }
+     TILE* src = (TILE*)tile_map;
+     TILE* dst0 = (TILE*)se_mem[SBB_0+0];
+     TILE* dst1 = (TILE*)se_mem[SBB_0+1];
+     TILE* dst2 = (TILE*)se_mem[SBB_0+2];
+     TILE* dst3 = (TILE*)se_mem[SBB_0+3];
+
+     // For each row copy to the appropriate SBB
+     for(int i = 0; i < 32; i++)
+     {
+
+         *(dst0++) = *(src++); 
+         *(dst0++) = *(src++); 
+
+         *(dst1++) = *(src++); 
+         *(dst1++) = *(src++); 
+
+     }
+
+     // For each row copy to the appropriate SBB
+     for(int i = 0; i < 32; i++)
+     {
+
+
+         *(dst2++) = *(src++); 
+         *(dst2++) = *(src++); 
+
+         *(dst3++) = *(src++); 
+         *(dst3++) = *(src++); 
+
+
+     }
+
+
+     //memcpy(&se_mem[SBB_0][0], tile_map, TEST_MAP_SIZE*sizeof(u16));
+     //memcpy(&se_mem[SBB_0][0], brinMap, brinMapLen);
+     //memcpy(&se_mem[SBB_0][0], test_map2Map, test_map2MapLen);
+
+     //    for(ii=0; ii<4; ii++)
+     //                for(jj=0; jj<32*32; jj++)
+     //                            *pse++= SE_PALBANK(ii) ;
+     //| 0;
 
 }
 
@@ -886,7 +1022,7 @@ int main()
                 rand()%(SCREEN_HEIGHT-16));
         character_list->add(heart);
         heart_list->add(heart);
-    
+
     }
 
 
