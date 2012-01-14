6ac262c1ce462971d990e97888b55f9be3b5a4d3
kyle2 <kyle2@ac2c0e10-25e9-44ee-ad31-64d50a0de64a>
2012-01-14 04:51:01 +0000

    Added some gfx tests.
    git-svn-id: file://localhost/home/share/Development/Subversion_repos/RazzleGBA@61 ac2c0e10-25e9-44ee-ad31-64d50a0de64a

---
+++ gfx/gummy_test.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(gummy_test)
+
+//======================================================================
+//
+//	gummy_test, 32x224@4, 
+//	+ palette 16 entries, not compressed
+//	+ 112 tiles not compressed
+//	Total size: 32 + 3584 = 3616
+//
+//	Time-stamp: 2009-04-08, 21:26:58
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_GUMMY_TEST_H
+#define GRIT_GUMMY_TEST_H
+
+#define gummy_testTilesLen 3584
+extern const unsigned int gummy_testTiles[896];
+
+#define gummy_testPalLen 32
+extern const unsigned short gummy_testPal[16];
+
+#endif // GRIT_GUMMY_TEST_H
+
+//}}BLOCK(gummy_test)
