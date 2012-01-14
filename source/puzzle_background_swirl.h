eacfc57556a591d710189e3768fa7c85dbe89575
kyle2 <kyle2@ac2c0e10-25e9-44ee-ad31-64d50a0de64a>
2012-01-14 04:47:46 +0000

    Added test stuff graphics
    git-svn-id: file://localhost/home/share/Development/Subversion_repos/RazzleGBA@59 ac2c0e10-25e9-44ee-ad31-64d50a0de64a

---
+++ source/puzzle_background_swirl.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(puzzle_background_swirl)
+
+//======================================================================
+//
+//	puzzle_background_swirl, 256x256@4, 
+//	+ palette 256 entries, not compressed
+//	+ 166 tiles (t|f|p reduced) not compressed
+//	+ regular map (in SBBs), not compressed, 32x32 
+//	Total size: 512 + 5312 + 2048 = 7872
+//
+//	Time-stamp: 2009-06-12, 16:37:17
+//	Exported by Cearn's GBA Image Transmogrifier
+//	( http://user.chem.tue.nl/jakvijn )
+//
+//======================================================================
+
+#ifndef __PUZZLE_BACKGROUND_SWIRL__
+#define __PUZZLE_BACKGROUND_SWIRL__
+
+#define puzzle_background_swirlPalLen 512
+extern const unsigned short puzzle_background_swirlPal[256];
+
+#define puzzle_background_swirlTilesLen 5312
+extern const unsigned int puzzle_background_swirlTiles[1328];
+
+#define puzzle_background_swirlMapLen 2048
+extern const unsigned short puzzle_background_swirlMap[1024];
+
+#endif // __PUZZLE_BACKGROUND_SWIRL__
+
+//}}BLOCK(puzzle_background_swirl)
